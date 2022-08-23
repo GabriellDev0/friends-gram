51c9989c080231d6919aa461971fa0dd613fd303
GabriellDev0 <gabrielldev0@gmail.com>
2022-08-23 19:12:12 -0300

    Corrigido um bug onde a contagem de views triggava na página do usuário

--- src/Components/Feed/FeedPostsItem.js
+++ src/Components/Feed/FeedPostsItem.js
@@ -2,11 +2,17 @@ import React from 'react'
 import Image from '../Helper/Image'
 import styles from './FeedPostsItem.module.css'
 import useFirebase from '../../Hooks/useFirebase'
+import { useLocation } from 'react-router-dom'
+
 const FeedPostsItem = ({post, setModalPost}) => {
+    const location = useLocation()
+ 
     const { setViewPost } = useFirebase()
     function handleClick(){
      setModalPost(post)
-     setViewPost(post.id)
+     if(location.key === "5yn1xexm"){
+      setViewPost(post.id)
+     } 
     }
   return (
     <li className={styles.post} onClick={handleClick}>
